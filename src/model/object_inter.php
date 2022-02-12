f91a6ab18e3ad5b06f0479bcacdf280c133ff77b
fhurtadoc <fabiohurtadoc@gmail.com>
2022-02-12 12:49:12 -0500

    estructura

---
+++ src/model/object_inter.php
@@ -0,0 +1,19 @@
+<?php
+
+interface Object_inter{
+
+    public function create();
+
+    public function delete();
+
+    public function update();
+
+    public function list();
+
+    public function findById();
+
+    public function findByparam();
+
+
+}
+
